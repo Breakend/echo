507214b51196aeacc9d52a56fb5fa4010d1888c7
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2014-11-30 16:33:04 -0500

    Disabled screen rotation so the app doesn't crash and removed unnecessary fragment on the main screen.

--- gen/com/example/android/wifidirect/R.java
+++ gen/com/example/android/wifidirect/R.java
@@ -36,32 +36,31 @@ public final class R {
         public static final int section_header=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001b;
-        public static final int atn_direct_discover=0x7f08001a;
-        public static final int atn_direct_enable=0x7f080019;
+        public static final int action_settings=0x7f08001a;
+        public static final int atn_direct_discover=0x7f080019;
+        public static final int atn_direct_enable=0x7f080018;
         public static final int btn_connect=0x7f080000;
         public static final int btn_disconnect=0x7f080001;
-        public static final int btn_send=0x7f080012;
-        public static final int btn_switch=0x7f08000e;
+        public static final int btn_send=0x7f080011;
+        public static final int btn_switch=0x7f08000d;
         public static final int device_address=0x7f080002;
-        public static final int device_details=0x7f080016;
+        public static final int device_details=0x7f080015;
         public static final int device_info=0x7f080003;
-        public static final int device_name=0x7f080015;
-        public static final int edit_message=0x7f080011;
+        public static final int device_name=0x7f080014;
+        public static final int edit_message=0x7f080010;
         public static final int frag_detail=0x7f08000c;
         public static final int frag_list=0x7f08000b;
-        public static final int frag_peers=0x7f08000d;
         public static final int group_ip=0x7f080005;
         public static final int group_owner=0x7f080004;
         public static final int icon=0x7f080008;
-        public static final int linearLayout1=0x7f08000f;
-        public static final int message_view=0x7f080010;
+        public static final int linearLayout1=0x7f08000e;
+        public static final int message_view=0x7f08000f;
         public static final int my_name=0x7f080009;
         public static final int my_status=0x7f08000a;
-        public static final int password=0x7f080014;
-        public static final int peer_MAC=0x7f080018;
-        public static final int peer_name=0x7f080017;
-        public static final int ssid=0x7f080013;
+        public static final int password=0x7f080013;
+        public static final int peer_MAC=0x7f080017;
+        public static final int peer_name=0x7f080016;
+        public static final int ssid=0x7f080012;
         public static final int status_bar=0x7f080006;
         public static final int status_text=0x7f080007;
     }
